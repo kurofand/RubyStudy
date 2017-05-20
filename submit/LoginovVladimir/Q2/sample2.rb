7ace88f2faf27ffd700c91374ea2f9cdde04f254
tuang <abc@gamil.com>
2017-05-20 15:23:24 +0900

    add sample and resolv 2017 may 20

---
+++ submit/LoginovVladimir/Q2/sample2.rb
@@ -0,0 +1,8 @@
+﻿hour=ARGV[0].to_i
+if((hour>=4)&&(hour<=10)) then
+	puts "おはよう"
+	elsif((hour>=11)&&(hour<=18)) then
+			puts "こんにちは"
+		elsif(((hour>=19)&&(hour<=24))||((hour>=1)&&(hour<=3))) then
+				puts "こんばんは"
+end
---
+++ submit/LoginovVladimir/Q2/sample3.rb
@@ -0,0 +1,17 @@
+﻿weight=ARGV[0].to_i
+height=ARGV[1].to_i
+bmi=weight/(height*height)
+case
+when bmi<18.5
+puts "低体重"
+when ((bmi>=18.5)&&(bmi<25))
+puts "普通体重"
+when ((bmi>=25)&&(bmi<30))
+puts "肥満（１度）"
+when ((bmi>=30)&&(bmi<35))
+puts "肥満（２度）"
+when ((bmi>=35)&&(bmi<40))
+puts "肥満（３度）"
+when bmi>=40
+puts "肥満（４度）"
+end
